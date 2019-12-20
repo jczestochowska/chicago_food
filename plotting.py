d7b3d41d556ec57ca006ba6ed93ec496b18f11a0
Bzdeco <jakub.gwizdala@gmail.com>
2019-12-20 19:32:07 +0100

    Plotting with multiple layers and sliders

--- plotting.py
+++ plotting.py
@@ -14,7 +14,7 @@ def insert_variables_as_properties_in_geodata(geodata, dataframe, key_property, 
         row = dataframe[dataframe[key_column] == key]
 
         if not row.empty:
-            variables_values = row[variable_names].values[0]  # todo: why this 0 index
+            variables_values = row[variable_names].values[0]
             for name, value in zip(variable_names, variables_values):
                 feature['properties'][name] = str(value)
     
@@ -34,40 +34,7 @@ def load_geodata(geodata_path, key_property):
             feature['id'] = feature['properties'][key_property]
         return geodata
 
-def get_center_coordinates(geodata):
-    # get neighborhoods latitudes and longitudes
-    centers = []
-    for area in geodata['features']:
-        area_coords = np.array(area['geometry']['coordinates'][0][0])
-        longitudes = area_coords[:, 0]
-        latitudes = area_coords[:, 1]
-        centers.append([0.5 * (longitudes.min() + longitudes.max()), 0.5 * (latitudes.min() + latitudes.max())])
-    centers = np.array(centers)
-    return centers[:, 0].mean(), centers[:, 1].mean()
-
 def plot_map(title, geodata_path, dataframe, key_property, key_column, value_column, template, mapbox_access_token, options):
-    '''
-    title: plot title
-    
-    geodata_path: path to the geojson file
-    
-    dataframe: Dataframe with columns: 
-      - zip (column name specified in zip_column),
-      - value plotted as color of the neighborhood (column name specified in value_column)
-      - any other additional column will be used as a variable in the template (first additional column as first
-      inter)
-    
-    zip_column: name of the column in the dataframe containing zip codes
-    
-    value_column: name of the column in the dataframe containig values for filling neighborhoods with colors
-    template: text that will be displayed in a box over the hovered neighborhood. Any variable in the template
-    should be denoted by "{}". Variables will be taken from the remaining columns of the dataframe in the order
-    in which the columns appear.
-    
-    template: a string in a python string format (with %s in places where to put variables)
-    
-    title: plot title
-    '''
     # unpack options
     colorscale = options['colorscale'] if 'colorscale' in options else 'Viridis'
     colorbar = options['colorbar'] if 'colorbar' in options else {}
@@ -97,24 +64,183 @@ def plot_map(title, geodata_path, dataframe, key_property, key_column, value_col
         title=title,
         font={'family': 'Arial Black'},
         mapbox_accesstoken=mapbox_access_token,
-        mapbox_zoom=9.5,
-        mapbox_pitch=40,
-        mapbox_bearing=70,
-        mapbox_center={'lat': 41.8781, 'lon': -87.6298},
+        mapbox_zoom=9,
+        mapbox_pitch=0,
+        mapbox_bearing=0,
+        mapbox_center={'lat': 41.86, 'lon': -87.63},
         margin={'r': 0, 'l': 50, 't': 50, 'b': 0}
     )
-    figure.show()
+    return figure
 
-def example_plot_map(mapbox_access_token):
-    # Put your mapbox token in variable of this name
-    chicago_geodata_path = 'data/chicago-zip.json'
+class MapLayer():
+    def __init__(self, name, dataframe, key_column, value_column, options):
+        self.name = name
+        self.dataframe = dataframe
+        self.key_column = key_column
+        self.value_column = value_column
+        self.options = options
+
+# Implementation based on: https://plot.ly/~empet/15237/choroplethmapbox-with-dropdown-menu/#/
+def plot_maps(title, geodata_path, key_property, layers, template, mapbox_access_token, options):    
+    # load geodata
+    geodata = load_geodata(geodata_path, key_property)
     
-    # Only to get some data for example dataframe
-    chicago_geodata = load_geodata(chicago_geodata_path, 'zip')
-    zips = [neighborhood['properties']['zip'] for neighborhood in chicago_geodata['features']]
+    data = []
+    for layer in layers:
+        # extract variables
+        dataframe = layer.dataframe
+        key_column = layer.key_column
+        value_column = layer.value_column
+        # unpack options
+        colorscale = layer.options['colorscale'] if 'colorscale' in options else 'Viridis'
+        colorbar = layer.options['colorbar'] if 'colorbar' in options else {}
+        showscale = layer.options['showscale'] if 'showscale' in options else True
+        
+        # prepare data
+        columns = dataframe.columns.values
+        dropped_columns = set([key_column, value_column])
+        variable_names = [column for column in columns if column not in dropped_columns]
+        extended_geodata = insert_variables_as_properties_in_geodata(geodata, dataframe, key_property, key_column, variable_names)
+        formatted_template = format_template(template, variable_names) + '<extra></extra>'
     
-    # Create example dataframe
-    df = pd.DataFrame({'zips': zips, 'values': zips, 'var1': np.arange(0, len(zips)), 'var2': np.arange(1, len(zips)+1)})
+        # plot
+        figure = go.Figure()
+        data.append(go.Choroplethmapbox(
+            geojson=extended_geodata,
+            locations=dataframe[key_column],
+            z=dataframe[value_column],
+            hovertemplate=formatted_template,
+            showscale=showscale,
+            colorscale=colorscale,
+            colorbar=colorbar,
+            visible=False
+        ))
     
-    # Plot map
-    plot_map('Chicago Neighborhoods', chicago_geodata_path, df, 'zip', 'zips', 'values', 'Some {} text {}', mapbox_access_token)
+    # mark first layer as visible
+    data[0]['visible'] = True
+    
+    # create layout
+    layout = go.Layout(
+        title=title,
+        font={'family': 'Arial Black'},
+        mapbox_accesstoken=mapbox_access_token,
+        mapbox_zoom=9,
+        mapbox_pitch=0,
+        mapbox_bearing=0,
+        mapbox_center={'lat': 41.86, 'lon': -87.63},
+        margin={'r': 0, 'l': 50, 't': 50, 'b': 0}
+        
+    )
+    
+    # create layers visibility lists
+    visibilities = []
+    template_visibility = np.zeros((len(layers),)).astype(bool)
+    for i in range(len(layers)):
+        visibility = list(np.copy(template_visibility))
+        visibility[i] = True
+        visibilities.append(visibility)
+    
+    # create menu buttons
+    buttons = []
+    for i, layer in enumerate(layers):
+        buttons.append({
+            'args': ['visible', visibilities[i]],
+            'label': layer.name,
+            'method': 'restyle'
+        })
+    
+    # update layout adding menu
+    layout.update(
+        updatemenus=[{
+            'x': 0.95,
+            'y': 0.95,
+            'yanchor': 'top',
+            'buttons': buttons
+        }]
+    )
+    
+    figure = go.Figure(data=data, layout=layout)
+    return figure
+
+def plot_maps_sliders(title, geodata_path, key_property, layers, template, mapbox_access_token, options):    
+    # load geodata
+    geodata = load_geodata(geodata_path, key_property)
+    
+    data = []
+    for layer in layers:
+        # extract variables
+        dataframe = layer.dataframe
+        key_column = layer.key_column
+        value_column = layer.value_column
+        # unpack options
+        colorscale = layer.options['colorscale'] if 'colorscale' in options else 'Viridis'
+        colorbar = layer.options['colorbar'] if 'colorbar' in options else {}
+        showscale = layer.options['showscale'] if 'showscale' in options else True
+        
+        # prepare data
+        columns = dataframe.columns.values
+        dropped_columns = set([key_column, value_column])
+        variable_names = [column for column in columns if column not in dropped_columns]
+        extended_geodata = insert_variables_as_properties_in_geodata(geodata, dataframe, key_property, key_column, variable_names)
+        formatted_template = format_template(template, variable_names) + '<extra></extra>'
+    
+        # plot
+        figure = go.Figure()
+        data.append(go.Choroplethmapbox(
+            geojson=extended_geodata,
+            locations=dataframe[key_column],
+            z=dataframe[value_column],
+            hovertemplate=formatted_template,
+            showscale=showscale,
+            colorscale=colorscale,
+            colorbar=colorbar,
+            visible=False
+        ))
+    
+    # mark first layer as visible
+    data[0]['visible'] = True
+    
+    # create layout
+    layout = go.Layout(
+        title=title,
+        font={'family': 'Arial Black'},
+        mapbox_accesstoken=mapbox_access_token,
+        mapbox_zoom=9,
+        mapbox_pitch=0,
+        mapbox_bearing=0,
+        mapbox_center={'lat': 41.86, 'lon': -87.63},
+        margin={'r': 0, 'l': 50, 't': 50, 'b': 0}
+        
+    )
+    
+    # create layers visibility lists
+    visibilities = []
+    template_visibility = np.zeros((len(layers),)).astype(bool)
+    for i in range(len(layers)):
+        visibility = list(np.copy(template_visibility))
+        visibility[i] = True
+        visibilities.append(visibility)
+    
+    # Create and add slider
+    steps = []
+    for i, layer in enumerate(layers):
+        step = {
+            'method': 'restyle',
+            'args': ['visible', visibilities[i]],
+            'label': layer.name
+        }
+        steps.append(step)
+
+    sliders = [{
+        'active': 0,
+        'currentvalue': {'prefix': 'Year: '},
+        'pad': {'t': 50},
+        'steps': steps
+    }]
+
+    layout.update(
+        sliders=sliders
+    )
+    
+    figure = go.Figure(data=data, layout=layout)
+    return figure
